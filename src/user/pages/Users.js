35b5c23196c6266489e85e5e441694f04836a687
imamfahrezi <imamfahrezi24@gmail.com>
2020-05-13 09:52:30 +0700

    adding a UerList Page Components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import UserList from '../components/UserList';
 
 function Users() {
-  return (
-    <div>
-      users work
-    </div>
-  )
+  const USERS = [
+    {
+      id: 'ul',
+      name: 'imam',
+      image:
+        'https://www.google.com/search?q=images+profile+picture&safe=strict&tbm=isch&source=iu&ictx=1&fir=5LJjGvPfcDwFSM%253A%252Cb5C9ViMmmhpq-M%252C_&vet=1&usg=AI4_-kSkEf6JBHqmt2sseDUd5o6n1Tyfew&sa=X&ved=2ahUKEwiR-6zr6a_pAhVFjeYKHb7SBYsQ9QEwDHoECAcQNA#imgrc=5LJjGvPfcDwFSM:',
+      places: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 }
 
-export default Users
+export default Users;
